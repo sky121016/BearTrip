f01fdc59748627d20e6a6f586f87f2d8bc84b08f
sky121016 <sky121016@naver.com>
2024-08-01 14:26:22 +0900

    continue

--- src/components/MyCalendar.jsx
+++ src/components/MyCalendar.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
+import { useNavigate } from "react-router-dom";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 
 const localizer = momentLocalizer(moment);
 
 const MyCalendar = (props) => {
+  const DragAndDropCalendar = withDragAndDrop(Calendar);
+
   const myEventsList = [
     {
       id: 0,
@@ -15,14 +19,41 @@ const MyCalendar = (props) => {
       end: new Date("2024-7-18"),
     },
   ];
+
+  const navigate = useNavigate();
+
+  const goSlot = (e) => {
+    console.log(e);
+    // navigate("/list")
+  };
+
+  const moveEvent = () => {
+    console.log("move");
+  };
+  
+  const resizeEvent = () => {
+    console.log("resize");
+  };
+
+  const handleSelectEvent = (e)=>{
+    console.log("select event");
+    console.log(e);
+
+    // show update event modal
+  }
+
   return (
     <div>
-      <Calendar
+      <DragAndDropCalendar
         localizer={localizer}
         events={myEventsList}
-        startAccessor="start"
-        endAccessor="end"
-        style={{width:800, height: 600 }}
+        onEventDrop={moveEvent}
+        onEventResize={resizeEvent}
+        onSelectSlot={goSlot}
+        onSelectEvent={handleSelectEvent}
+        selectable
+        resizable
+        style={{ width: 800, height: 600 }}
       />
     </div>
   );
